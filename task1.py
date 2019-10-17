00209d275895ba940bda338003873fa2904724ee
vandan rohatgi <43648786+vandanrohatgi@users.noreply.github.com>
2019-10-17 19:44:57 +0530

    Update task1.py

--- task1.py
+++ task1.py
@@ -4,7 +4,7 @@ import random
 listofmovies=['avengers endgame','star wars the force awakens','avengers infinity war','jurassic world','the lion king','furious seven','black panther','harry potter',
 'frozen','beauty and the beast','incredibles two','iron man three','minions','aquaman','aladdin','finding dory','zootopia','spectre','spider man homecoming','batman v superman','hunger games'
 ]
-#list of all vowels to remove everythong except them from the name of the movie 
+#list of all vowels to remove everything except them from the name of the movie 
 vowels=['a','e','i','o','u']
 #list of alphabets of 'hollywood' to cut them one by one everytime the user makes a wrong guess
 string=['h','o','l','l','y','w','o','o','d']
@@ -17,7 +17,7 @@ first we remove the spaces from the name of movie using the split function which
 remove_spaces=choosen.split(' ')
 
 '''secondly we take the previous variable and put a '/' where there was a space in the name using the join function
-if you feel it is tooamny functions, don't fret just head over to python docs to read about every function that I have used here'''
+if you feel it is too many functions, don't fret just head over to python docs to read about every function that I have used here'''
 
 choosen='/'.join(remove_spaces)
 
